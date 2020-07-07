8b41576176f9a02ac65d73cf27f90717773ef535
janLti <japi@gmx.ch>
2020-07-07 21:31:38 +0200

    Added: initial project

---
+++ src/test/java/org/jlue/drills/SimpleStackTest.java
@@ -0,0 +1,14 @@
+package org.jlue.drills;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SimpleStackTest {
+
+    @Test
+    public void pushPopTest() {
+        SimpleStack<Integer> testObj = new SimpleStack<Integer>();
+        testObj.push(1);
+        Assert.assertEquals(1, testObj.pop().intValue());
+    }
+}
